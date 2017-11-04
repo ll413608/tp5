04cd9668cb05d04ec5ffdd30183e1efe53b03a0a
xingming.wang <xingming.wang@qq.com>
2017-11-04 22:57:14 +0800

    testCode

---
+++ application/index/controller/Index.php
@@ -0,0 +1,36 @@
+<?php
+namespace app\index\controller;
+
+use \think\Log;
+use \think\Db;
+
+class Index extends \think\Controller
+{
+    public function index()
+    {
+        $data = Db::table('TestInfo')->order('id desc')->limit(100)->select();
+        var_dump($data);
+        return url();
+    }
+
+    public function test()
+    {
+        return $this->fetch();
+    }
+
+    public function insert()
+    {
+        $dataInfo = ['type' => 'test呀', 'info' => '测试'];
+        $result = Db::table('TestInfo')->insert($dataInfo);
+        return $result;
+    }
+
+    public function job()
+    {
+        for($i = 0; $i < 100000; $i++) {
+            $data = '任务生产时间：' . date('Y-m-d H:i:s');
+            \think\Queue::push('index/Job1', $data = $data, $queue = null);
+        }
+        return '任务已经生产完毕';
+    }
+}
